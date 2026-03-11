bd217290de76ce066561a028c184da2be156cbf5
nikored69 <redhand1912@gmail.com>
2026-03-11 22:25:24 +0300

    game.js

---
+++ game.js
@@ -0,0 +1,3 @@
+console.log("🚀 Telegram данные:", window.Telegram?.WebApp?.initData);
+console.log("📱 Платформа:", window.Telegram?.WebApp?.platform);
+console.log("🎮 Игра готова:", window.Telegram?.WebApp.initDataUnsafe?.game?.title);
